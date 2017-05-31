0b93a94afb6b9982ea9ac5c0d3c328881407b2da
Kaushik M K <mkkaushiksjce@gmail.com>
2017-05-31 21:41:44 +0530

    parallax scrolling implemneted

---
+++ app/js/app.js
@@ -0,0 +1,14 @@
+// carousel code
+$(document).ready(function () {
+    // $('.owl-carousel').owlCarousel({
+    //     loop: true,
+    //     nav: false,
+    //     items: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 1000,
+    //     animateOut: 'fadeOut'
+    // });
+
+});
+
+
